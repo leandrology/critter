e1556d75acf6b57b10b29dd57c19a71e01da8617
leandrology <leandrology@gmail.com>
2022-05-08 17:31:35 +0800

    commit

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-3xl font-bold underline">Leander!</h1>
+      <h1 className="text-3xl font-bold underline">Leander Josh!</h1>
     </div>
   );
 }
